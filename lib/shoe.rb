be5ab52e321bc75c8c084d6f3543bb8030dad13f
Nory Lazo <nlazo@icloud.com>
2019-11-21 06:35:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,11 +7,9 @@ class Shoe
   def initialize(brand)
     @brand = brand
     
-    BRANDS.each do |brand|
       if BRANDS.include?(brand) == false
         BRANDS << brand
       end
-    end 
   end
 
   def cobble
